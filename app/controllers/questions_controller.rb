92262b8819253a7d85f49cb8a3370e9a8a218b10
Chris Fraz & Elsie Koglin <pair+cfrazz25.elsiekoglin@devbootcamp.com>
2017-04-17 17:00:07 -0500

    stuff

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,37 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(params[:question])
+    @question.user_id = session[:user_id]
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find_by(id: params[:id])
+  end
+
+  def edit
+    @question = Question.find_by(id: params[:id])
+  end
+
+  def update
+    @question = Question.find_by(id: params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+   # render :new
+  end
 
+  def create
+    @user = User.authenticate(params[:username], params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to question_path
+    else
+      @errors = ["That user does not exist."]
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to sessions_path
+  end
 end
